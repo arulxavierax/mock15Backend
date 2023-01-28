d60f77badb5490eb6a755ef42bec39383ac852e9
arul xavier <arulxavier2121@gmail.com>
2023-01-28 11:54:15 +0530

    mock15 backend

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("hello"));
 
-app.listen(8080, () => {
-  console.log("server started on port 8080");
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
